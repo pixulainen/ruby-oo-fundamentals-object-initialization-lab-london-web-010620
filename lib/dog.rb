b00400dcca0cfacbea23b0bf89733efd2d36bf1c
George Apetrei <george.apetrei@gmail.com>
2020-01-07 12:14:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,18 @@
 def Dog 
   
-  def initialize(breed="Mutt")
+  def initialize(name,breed="Mutt")
     @breed= breed
+    @name = name
   end
-  
+  def name=(name)
+    @name = name
+  end
   def breed=(breed)
     @breed= breed
   end
-  
+  def name 
+    @name
+  end
   def breed
     @breed
   end
